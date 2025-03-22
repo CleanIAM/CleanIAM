696f0c2579d960b93bb595c4b9e045a089256849
Gwynbleid85 <hegr.milos85@seznam.cz>
2025-03-22 15:28:08 +0100

    feat: update password hashing

--- src/Identity/Application/Services/PasswordService.cs
+++ src/Identity/Application/Services/PasswordService.cs
@@ -9,17 +9,32 @@ public class PasswordService : IPasswordService
 {
     private readonly Argon2id _argon;
 
+    // https://www.rfc-editor.org/rfc/rfc9106.html#name-parameter-choice
+    private const int SaltSize = 16;
+    private const int HashSize = 128;
+
     public PasswordService()
     {
         // Configuration for `Argon2id` algorithm is based on OWASP recommendations:
         // https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
         var argon = PasswordBasedKeyDerivationAlgorithm.Argon2id(new Argon2Parameters()
         {
-            MemorySize = 19 * 1024 * 1024, // 19MB
+            MemorySize = 19456, // 19MB (value is in kibibytes)
             NumberOfPasses = 2, // iteration count of 2
             DegreeOfParallelism = 1 // Single degree of parallelism
         });
         _argon = argon;
+    }
+
+    public PasswordHash Hash(string password)
+    {
+        var salt = RandomNumberGenerator.GetBytes(SaltSize);
+
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+
+        var hash = _argon.DeriveBytes(passwordBytes, salt, 128);
+
+        return new PasswordHash(hash, salt);
     }
 
     public bool Compare(string password, PasswordHash hash)
@@ -27,18 +42,6 @@ public class PasswordService : IPasswordService
         var passwordBytes = Encoding.UTF8.GetBytes(password);
 
         var hashBytes = _argon.DeriveBytes(passwordBytes, hash.Salt, 128);
-
-        return hashBytes.SequenceEqual(hash.Hash);
-    }
-
-    public PasswordHash Hash(string password)
-    {
-        var salt = RandomNumberGenerator.GetBytes(32);
-
-        var passwordBytes = Encoding.UTF8.GetBytes(password);
-
-        var hash = _argon.DeriveBytes(passwordBytes, salt, 128);
-
-        return new PasswordHash(hash, salt);
+        return CryptographicOperations.FixedTimeEquals(hashBytes, hash.Hash);
     }
 }
