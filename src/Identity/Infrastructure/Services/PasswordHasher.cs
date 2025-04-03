b421c0899a6ff5a348d53371c7e60d8631c0135b
Milos Hegr <hegr.milos85@seznam.cz>
2025-04-03 11:30:51 +0200

    feat: add phc string to HashedPassword structure

--- src/Identity/Infrastructure/Services/PasswordHasher.cs
+++ src/Identity/Infrastructure/Services/PasswordHasher.cs
@@ -6,6 +6,8 @@ using NSec.Cryptography;
 
 namespace Identity.Infrastructure.Services;
 
+
+
 public class PasswordHasher : IPasswordHasher
 {
     private readonly Argon2id _argon;
@@ -13,16 +15,24 @@ public class PasswordHasher : IPasswordHasher
     // https://www.rfc-editor.org/rfc/rfc9106.html#name-parameter-choice
     private const int SaltSize = 16;
     private const int HashSize = 128;
+ 
+    // Configuration for `Argon2id` algorithm is based on OWASP recommendations:
+    // https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+    private const int MemorySize = 19456; // 19MB (value is in kibibytes)
+    private const int NumberOfPasses = 2; // iteration count of 2
+    private const int DegreeOfParallelism = 1; // Single degree of parallelism
+    
+    private const int AlgorithmVersion = 0x13; // Argon2id version 0x13
+    
+    
 
     public PasswordHasher()
     {
-        // Configuration for `Argon2id` algorithm is based on OWASP recommendations:
-        // https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
-        var argon = PasswordBasedKeyDerivationAlgorithm.Argon2id(new Argon2Parameters()
+       var argon = PasswordBasedKeyDerivationAlgorithm.Argon2id(new Argon2Parameters()
         {
-            MemorySize = 19456, // 19MB (value is in kibibytes)
-            NumberOfPasses = 2, // iteration count of 2
-            DegreeOfParallelism = 1 // Single degree of parallelism
+            MemorySize = MemorySize,
+            NumberOfPasses = NumberOfPasses,
+            DegreeOfParallelism = DegreeOfParallelism
         });
         _argon = argon;
     }
@@ -35,11 +45,22 @@ public class PasswordHasher : IPasswordHasher
 
         var hash = _argon.DeriveBytes(passwordBytes, salt, 128);
 
-        return new HashedPassword(hash, salt);
+        var hashAlgorithmSignature =
+            $"$argon2id$v={AlgorithmVersion}$m={MemorySize},t={NumberOfPasses},p={DegreeOfParallelism}";
+    
+        return new HashedPassword(hash, salt, hashAlgorithmSignature);
     }
 
     public bool Compare(string password, HashedPassword hash)
     {
+        var hashAlgorithmSignature =
+            $"$argon2id$v={AlgorithmVersion}$m={MemorySize},t={NumberOfPasses},p={DegreeOfParallelism}";
+
+        // NOTE: If new the hash algorithm is used, here is the place to check it
+        if (hash.HashAlgorithmSignature != hashAlgorithmSignature)
+            throw new InvalidOperationException(
+                $"Hash algorithm signature mismatch. Expected: {hashAlgorithmSignature}, Actual: {hash.HashAlgorithmSignature}");
+        
         var passwordBytes = Encoding.UTF8.GetBytes(password);
 
         var hashBytes = _argon.DeriveBytes(passwordBytes, hash.Salt, 128);
