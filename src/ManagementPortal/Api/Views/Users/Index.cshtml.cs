9dca7ce0dcc644f38dee5438a142a861370d2829
Milos Hegr <hegr.milos85@seznam.cz>
2025-04-18 20:04:22 +0200

    feat: add user management frontend

--- src/ManagementPortal/Api/Views/Users/Index.cshtml.cs
+++ src/ManagementPortal/Api/Views/Users/Index.cshtml.cs
@@ -1,19 +1,9 @@
-using ManagementPortal.Application.Queries.Users;
 using ManagementPortal.Core;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Wolverine;
 
 namespace ManagementPortal.Api.Views.Users;
 
-public class Index(IMessageBus bus) : PageModel
+public class UserPageModel
 {
-    public User[] Users { get; set; }
-
-    public async Task OnGetAsync()
-    {
-        var query = new GetAllUsersQuery();
-        var users = await bus.InvokeAsync<IEnumerable<User>>(query);
-
-        Users = users.ToArray();
-    }
+    public required User[] Users { get; set; }
 }
