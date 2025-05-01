0c61dabc70208b1df9ae072d412b9ac839a45c0d
Gwynbleid85 <hegr.milos85@seznam.cz>
2025-05-01 17:56:07 +0200

    feat: add docs for url shortener

--- src/UrlShortener/Api/Controllers/UrlShortnerController.cs
+++ src/UrlShortener/Api/Controllers/UrlShortnerController.cs
@@ -6,17 +6,25 @@ using Wolverine;
 
 namespace UrlShortner.Api.Controllers;
 
-public class UrlShortnerController(IMessageBus bus): Controller
+/// <summary>
+/// The main controller for the URL shortener.
+/// </summary>
+/// <param name="bus"></param>
+public class UrlShortnerController(IMessageBus bus) : Controller
 {
-    
+    /// <summary>
+    /// The endpoint handling the shortened url access
+    /// </summary>
     [HttpGet("/url-shortner/{id:guid}")]
     public async Task<IActionResult> GetUrl(Guid id)
     {
         var query = new GetShortenedUrlByIdQuery(id);
         var url = await bus.InvokeAsync<ShortenedUrl?>(query);
-        if(url == null)
-            return View("Error", new ErrorViewModel{Error = "Wrong url", ErrorDescription = "This url goes nowhere" });
+        
+        if (url == null)
+            return View("Error",
+                new ErrorViewModel { Error = "Wrong url", ErrorDescription = "This url goes nowhere" });
+        
         return Redirect(url.OriginalUrl);
     }
-    
 }
