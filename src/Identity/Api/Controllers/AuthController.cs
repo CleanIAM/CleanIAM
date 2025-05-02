2d591019f5b894ca6cfc18b93f0e8968f38ba424
Gwynbleid85 <hegr.milos85@seznam.cz>
2025-05-02 18:17:34 +0200

    feat: end session flow update

--- src/Identity/Api/Controllers/AuthController.cs
+++ src/Identity/Api/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 using Identity.Api.ViewModels.Auth;
+using Identity.Api.ViewModels.Shared;
 using Identity.Application.Interfaces;
 using Identity.Core.Requests;
 using Microsoft.AspNetCore;
@@ -10,20 +11,29 @@ using OpenIddict.Server.AspNetCore;
 
 namespace Identity.Api.Controllers;
 
+/// <summary>
+/// Main Controller for handling OAuth2 and OpenId Connect requests.
+///
+/// Some parts of the OAuth 2 flow are handled by OpenIddict itself. <see cref="DependencyInjection.AddOpenIddict"/>
+/// </summary>
 [Route("/connect")]
 public class AuthController(
     ISigninRequestService signinRequestService,
     IIdentityBuilderService identityBuilderService)
     : Controller
 {
+    /// <summary>
+    /// The main endpoint for OAuth 2 authorization code flow.
+    ///
+    /// If the user is not authenticated, the user will be redirected to the signin page.
+    /// </summary>
     [HttpGet("authorize")]
     public async Task<IActionResult> Authorize()
     {
-        //TODO: Refactor identity creation
         var oidcRequest = HttpContext.GetOpenIddictServerRequest();
         if (oidcRequest == null)
             // TODO: Create error page and redirect to it
-            return BadRequest(new OpenIddictResponse
+            return View("error", new ErrorViewModel
             {
                 Error = OpenIddictConstants.Errors.InvalidRequest,
                 ErrorDescription = "The OpenID Connect request cannot be retrieved."
@@ -54,6 +64,10 @@ public class AuthController(
         return SignIn(principal.Value, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
     }
 
+    /// <summary>
+    /// Show the view to confirm the consent of the user to sing out.
+    /// </summary>
+    /// <returns></returns>
     [HttpGet("endsession")]
     public IActionResult EndSession()
     {
@@ -64,24 +78,42 @@ public class AuthController(
         });
     }
 
-
+    /// <summary>
+    /// Endpoint handling the sign-out request.
+    /// </summary>
     [HttpPost("endsession")]
-    public async Task<IActionResult> EndSession(OpenIddictRequest request)
+    public async Task<IActionResult> EndSession(OpenIddictRequest request, EndSessionViewModel? model)
     {
         var oidcRequest = HttpContext.GetOpenIddictServerRequest();
 
-        // Sign out from local identity server session
-        await HttpContext.SignOutAsync();
+        // Sign out from a local identity server session
+        if (model is not null && model.FullLogout)
+            await HttpContext.SignOutAsync();
 
-        // Sign out from OpenIddict session
+        // Sign out from the OpenIddict session
         return SignOut(
             authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
             properties: new AuthenticationProperties
             {
-                RedirectUri = oidcRequest?.RedirectUri ?? "/",
+                // The `oidcRequest?.PostLogoutRedirectUri` is already validated by OpenIddict.
+                RedirectUri = oidcRequest?.PostLogoutRedirectUri ?? "/connect/endsession/success"
             });
     }
 
+    /// <summary>
+    /// Show the view to confirm the consent of the user to sing out.
+    ///
+    /// Just a fallback in case the client application did not provide a redirect URI.
+    /// </summary>
+    [HttpGet("endsession/success")]
+    public IActionResult EndSessionSuccess()
+    {
+        return View();
+    }
+
+    /// <summary>
+    /// The main endpoint for OpenId Connect userinfo requests.
+    /// </summary>
     [HttpGet("userinfo")]
     [HttpPost("userinfo")]
     [Produces("application/json")]
