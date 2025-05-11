ea3ecbb4a0a697e93cf5b503c37d84588118ba1b
Milos Hegr <hegr.milos85@seznam.cz>
2025-05-11 12:11:48 +0200

    feat: add status check endpoint

---
+++ src/SharedKernel/Api/Controllers/StatusCheckController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharedKernel.Api.Controllers;
+
+/// <summary>
+/// Controller for maintenance endpoints.
+/// </summary>
+public class StatusCheckController: Controller
+{
+    /// <summary>
+    /// Get app health status
+    /// </summary>
+    [AllowAnonymous]
+    [HttpGet("/healthz")]
+    public static IResult GetHealthStatus()
+    {
+        return Results.Ok();
+    }
+
+    /// <summary>
+    /// Get app ready status
+    /// </summary>
+    [AllowAnonymous]
+    [HttpGet("/readyz")]
+    public static IResult GetReadyStatus()
+    {
+        return Results.Ok();
+    }
+}
